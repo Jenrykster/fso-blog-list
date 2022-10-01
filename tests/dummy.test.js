5b9cc47a6ceedec1c063fab31450f55ca1720c28
João Henrique <joaoenrique.san@gmail.com>
2022-10-01 12:38:24 -0300

    test: write tests for the favoriteBlog helper

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,5 +1,14 @@
 const listHelper = require('../utils/list_helper');
 
+const exampleBlog = {
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  likes: 5,
+  __v: 0,
+};
+
 test('dummy returns one', () => {
   const blogs = [];
 
@@ -8,15 +17,6 @@ test('dummy returns one', () => {
 });
 
 describe('total likes', () => {
-  const exampleBlog = {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  };
-
   test('of empty list is zero', () => {
     expect(listHelper.totalLikes([])).toBe(0);
   });
@@ -32,5 +32,36 @@ describe('total likes', () => {
     const biggerList = [exampleBlog, exampleBlog2, exampleBlog2, exampleBlog, exampleBlog2];
 
     expect(listHelper.totalLikes(biggerList)).toBe(40);
+  });
+});
+
+describe.only('favorite blog', () => {
+  test('of empty list is undefined', () => {
+    expect(listHelper.favoriteBlog([])).toBe(undefined);
+  });
+
+  test('when list has only one blog is that same blog', () => {
+    const expectedAnswer = {
+      title: exampleBlog.title,
+      author: exampleBlog.author,
+      likes: exampleBlog.likes,
+    };
+
+    expect(listHelper.favoriteBlog([exampleBlog])).toEqual(expectedAnswer);
+  });
+
+  test('of a list with multiple blogs is the one with the most likes', () => {
+    const exampleBlog2 = { ...exampleBlog, likes: 20 };
+    const exampleBlog3 = { ...exampleBlog, likes: 50 };
+
+    const expectedAnswer = {
+      title: exampleBlog3.title,
+      author: exampleBlog3.author,
+      likes: exampleBlog3.likes,
+    };
+
+    expect(listHelper.favoriteBlog([exampleBlog, exampleBlog2, exampleBlog3])).toEqual(
+      expectedAnswer,
+    );
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,26 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs = []) => blogs.reduce((prev, curr) => prev + curr.likes, 0);
 
+const favoriteBlog = (blogs = []) => {
+  if (blogs.length === 0) return undefined;
+
+  const initialValue = {
+    title: '',
+    author: '',
+    likes: 0,
+  };
+
+  return blogs.reduce((prev, curr) => {
+    if (prev.likes > curr.likes) return prev;
+    return {
+      title: curr.title,
+      author: curr.author,
+      likes: curr.likes,
+    };
+  }, initialValue);
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
